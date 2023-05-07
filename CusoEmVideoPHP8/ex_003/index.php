d1fcb8d7d1e5db1a3e7da002458040fb64657aca
Carlos <carlos.admcode@gmail.com>
2023-05-07 19:45:45 -0300

    Iniciando curso e realizando exercicios 0 ao 3

---
+++ CusoEmVideoPHP8/ex_003/index.php
@@ -0,0 +1,11 @@
+<?php
+    $nome = "Gustavo";
+    $sobrenome = "Guanabara";
+    //
+    const PAIS = "Brasil";
+    //
+    $nome = "Rafael";
+    $sobrenome = "Silva";
+    //
+    print "Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
+?>
