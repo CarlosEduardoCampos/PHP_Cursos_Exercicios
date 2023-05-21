f74296cc3d6e1ca78f27d60aed6b7e3d8245b1d3
Carlos <carlos.admcode@gmail.com>
2023-05-21 15:09:54 -0300

    Retira senhas

--- HcodePHP7/PDO/mysql_connect.php
+++ HcodePHP7/PDO/mysql_connect.php
@@ -1,15 +1,9 @@
 <?php
     # Variaveis para conexão do Banco de Dados:
-    /*
     define('DB_NAME', '');
     define('DB_HOST', '');
     define('DB_USER', '');
     define('DB_PASS', '');
-    */
-    define('DB_NAME', 'php7');
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'testeBanco123');
 
     # Conexãocom Banco de Dados:
     $conn = new PDO("mysql:dbname=".DB_NAME.";host=".DB_HOST, DB_USER, DB_PASS);
